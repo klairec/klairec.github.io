1cf29fdf4b1b2b35ca20a5525d541773970c47d6
clairek33 <claire.krucker@hotmail.fr>
2017-08-21 22:13:36 +0200

    modif resp

--- js/app.js
+++ js/app.js
@@ -3,14 +3,21 @@ $(function($){
     // je créé une variable qui contient le tableau des différentes sections que j'ai dans mon body
  	var sections = [];
 
- 	// je créé une variable qui va stocker l'id de la section scrollée
- 	var id = false;
+    // je créé une variable qui va stocker l'id de la section scrollée
+    var id = false;
 
+    // je créé une variable qui récupère la largeur de l'écran
+    var iWindowsSize = $(window).width();’
 
  	// je créé une fonction permettant d'ajouter une animation au click des liens du menu
  	$('.sidebar a').click(function(e){
- 		$('html, body').animate(
-            { scrollTop: ($($(this).attr('href'))).offset().top }, 600);
+        if (iWindowsSize < 1024){
+     		$('html, body').animate(
+                { scrollTop: ($($(this).attr('href'))).offset().top-230 }, 1200);
+        } else {
+            $('html, body').animate(
+                { scrollTop: ($($(this).attr('href'))).offset().top}, 1200);
+        }
  	});
 
 
